3b59ae3be6a7905055253beb50ecd8eb05066b07
刘杰 <jie.liu07@hand-china.com>
2019-01-18 19:49:47 +0800

    third commit

---
+++ gateway/src/main/java/com/zzrq/config/ServletContextConfig.java
@@ -0,0 +1,8 @@
+package com.zzrq.config;
+
+import com.zzrq.base.config.BaseServletContextConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServletContextConfig extends BaseServletContextConfig {
+}
